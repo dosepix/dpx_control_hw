946a413146a9c6b8b8c88704a4b6e03a3e92607a
dosepix <schm.seb@gmail.com>
2022-03-24 16:26:24 +0100

    Added plot to measure/measure_tot

--- measure/measure_tot.py
+++ measure/measure_tot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import dpx_control_hw as dch
+import numpy as np
+import matplotlib.pyplot as plt
 
 PORT = '/dev/ttyACM0'
 def main():
@@ -9,19 +11,25 @@ def main():
     params_fn = None
     dpx = dch.Dosepix(
         port_name=PORT,
-        config_fn=config_fn,
+        config_fn='config.conf',
         thl_calib_fn=thl_calib_fn,
         params_fn=params_fn,
         bin_edges_fn=bin_edges
     )
 
-    dpx.dpf.measure_tot(
+    tot_frames = dpx.dpf.measure_tot(
         frame_time=0,
         save_frames=None,
         out_dir='tot_measurement/',
-        meas_time=5,
+        meas_time=1,
         use_gui=False
     )
+
+    # Plot
+    tot = np.asarray(tot_frames).flatten()
+    tot = tot[tot > 0]
+    plt.hist(tot, bins=np.arange(1000))
+    plt.show()
 
 if __name__ == '__main__':
     main()
